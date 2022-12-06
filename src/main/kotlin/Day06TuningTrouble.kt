28003422e9276522726f2ebeb79a660762781c65
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-06 06:20:47 +0100

    Implemented isStartOfPackageMarker

--- src/main/kotlin/Day06TuningTrouble.kt
+++ src/main/kotlin/Day06TuningTrouble.kt
@@ -5,4 +5,15 @@ class TuningTrouble {
 
     fun loadData(path: Path) : String = path.readLines()[0]
 
+    fun createWindows(dataStream: String): List<String> = dataStream.windowed(size = 4, step = 1)
+
+    fun isStartOfPacketMarker(dataPackage: String): Boolean {
+        var count = 0
+        count += dataPackage.count { it == dataPackage[0] }
+        count += dataPackage.count { it == dataPackage[1] }
+        count += dataPackage.count { it == dataPackage[2] }
+        count += dataPackage.count { it == dataPackage[3] }
+        return count == 4
+    }
+
 }
--- src/test/kotlin/Day06TuningTroubleTest.kt
+++ src/test/kotlin/Day06TuningTroubleTest.kt
@@ -21,12 +21,34 @@ class TuningTroubleTest {
         val dataStream = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
         // act
-        val windows = dataStream.windowed(size = 4, step = 1)
+        val windows = TuningTrouble().createWindows(dataStream)
 
         // assert
         assertThat(windows).hasSize(27)
         assertThat(windows[0]).isEqualTo("mjqj")
         assertThat(windows[1]).isEqualTo("jqjp")
+    }
+
+    @Test
+    fun isStartOfPacketMarker_mjgj_false() {
+        // arrange
+
+        // act
+        val startOfPacketMarker = TuningTrouble().isStartOfPacketMarker("mjqj")
+
+        // assert
+        assertThat(startOfPacketMarker).isFalse
+    }
+
+    @Test
+    fun isStartOfPacketMarker_mjgx_true() {
+        // arrange
+
+        // act
+        val startOfPacketMarker = TuningTrouble().isStartOfPacketMarker("mjqx")
+
+        // assert
+        assertThat(startOfPacketMarker).isTrue
     }
 
 }
