93371573442f5386ee2ac1d307406ab68de180d9
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-08 22:46:49 +0100

    Checked for row and column

--- src/main/kotlin/Day08TreetopTreeHouse.kt
+++ src/main/kotlin/Day08TreetopTreeHouse.kt
@@ -1,3 +1,12 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
 class TreetopTreeHouse {
+
+    fun loadData(path: Path) : List<String> = path.readLines()
+
+    fun convertToInts(input: List<String>): List<List<Int>> {
+        return input.map { s -> s.map { it.digitToInt() } }
+    }
 
 }
--- src/test/kotlin/Day08TreetopTreeHouseTest.kt
+++ src/test/kotlin/Day08TreetopTreeHouseTest.kt
@@ -1,3 +1,52 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
 class TreetopTreeHouseTest {
 
+    @Test
+    fun convertToInts() {
+        // arrange
+        val inputAsString = listOf(
+            "30373",
+            "25512",
+            "65332",
+        )
+
+        // act
+        val inputAsInt = TreetopTreeHouse().convertToInts(inputAsString)
+
+        // assert
+        assertThat(inputAsInt[0].size).isEqualTo(5)
+        assertThat(inputAsInt.size).isEqualTo(3)
+    }
+
+    @Test
+    fun loadData() {
+
+        // act
+        val rawData = TreetopTreeHouse().loadData(Path("src", "test", "resources", "Day08_TestData.txt"))
+
+        // assert
+        assertThat(rawData).hasSize(5)
+    }
+
+    //    30373
+//    25512
+//    65332
+//    33549
+//    35390
+    @Test
+    fun loadAndConvertData() {
+        // arrange
+        val treetopTreeHouse = TreetopTreeHouse()
+        val rawData = treetopTreeHouse.loadData(Path("src", "test", "resources", "Day08_TestData.txt"))
+
+        // act
+        val inputAsInt = treetopTreeHouse.convertToInts(rawData)
+
+        // assert
+        assertThat(inputAsInt[0][3]).isEqualTo(7)
+
+    }
 }
