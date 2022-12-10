7646efe786ff5fda0d5866d82ec6b50322bdaff8
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-10 06:40:29 +0100

    Implemented cycles

--- src/main/kotlin/Day10CathodeRayTube.kt
+++ src/main/kotlin/Day10CathodeRayTube.kt
@@ -3,6 +3,26 @@ import kotlin.io.path.readLines
 
 class CathodeRayTube {
 
+    var X: Int = 1
+
     fun loadData(path: Path): List<String> = path.readLines()
+
+    fun cycle(instruction: String): List<Int> {
+        if (instruction == "noop") {
+            return listOf(X)
+        }
+        val value = instruction.split(" ")[1].toInt()
+        val cycles = listOf(X, X)
+        X += value
+        return cycles
+    }
+
+    fun cycles(instructions: List<String>): List<Int> {
+        val cycles = mutableListOf<Int>()
+        for (instruction in instructions) {
+            cycles.addAll(cycle(instruction))
+        }
+        return cycles
+    }
 
 }
--- src/test/kotlin/Day10CathodeRayTubeTest.kt
+++ src/test/kotlin/Day10CathodeRayTubeTest.kt
@@ -4,14 +4,65 @@ import kotlin.io.path.Path
 
 class CathodeRayTubeTest {
 
+    private val cathodeRayTube = CathodeRayTube()
+
     @Test
     fun loadData() {
 
         // act
-        val rawData = CathodeRayTube().loadData(Path("src", "test", "resources", "Day10_TestData.txt"))
+        val rawData = cathodeRayTube.loadData(Path("src", "test", "resources", "Day10_TestData.txt"))
 
         // assert
         assertThat(rawData).hasSize(146)
+    }
+
+    @Test
+    fun cycle_noop() {
+
+        // act
+        val cycles = cathodeRayTube.cycle("noop")
+
+        // assert
+        assertThat(cycles).containsExactly(1)
+    }
+
+    @Test
+    fun cycle_addx3() {
+
+        // act
+        val cycles = cathodeRayTube.cycle("addx 3")
+
+        // assert
+        assertThat(cycles).containsExactly(1, 1)
+        assertThat(cathodeRayTube.X).isEqualTo(4)
+    }
+
+    @Test
+    fun cycle_addMinus5() {
+
+        // act
+        val cycles = cathodeRayTube.cycle("addx -5")
+
+        // assert
+        assertThat(cycles).containsExactly(1, 1)
+        assertThat(cathodeRayTube.X).isEqualTo(-4)
+    }
+
+    @Test
+    fun cycle_smallProgramm() {
+        // arrange
+        val instructions = listOf(
+            "noop",
+            "addx 3",
+            "addx -5"
+        )
+
+        // act
+        val cycles = cathodeRayTube.cycles(instructions)
+
+        // assert
+        assertThat(cycles).containsExactly(1, 1, 1, 4, 4)
+        assertThat(cathodeRayTube.X).isEqualTo(-1)
     }
 
 }
