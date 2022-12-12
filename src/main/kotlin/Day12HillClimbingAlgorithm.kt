9020d9ccfce56269b85995301eef85014aa47fd5
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-12 06:18:53 +0100

    Implemented load data

--- src/main/kotlin/Day12HillClimbingAlgorithm.kt
+++ src/main/kotlin/Day12HillClimbingAlgorithm.kt
@@ -1,3 +1,11 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
 class HillClimbingAlgorithm {
+
+    fun loadData(path: Path): List<List<Char>> {
+        val rawData =  path.readLines()
+        return rawData.map { line -> line.map { it } }
+    }
 
 }
--- src/test/kotlin/Day12HillClimbingAlgorithmTest.kt
+++ src/test/kotlin/Day12HillClimbingAlgorithmTest.kt
@@ -1,3 +1,19 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
 class HillClimbingAlgorithmTest {
+
+    @Test
+    fun loadData() {
+        // arrange
+
+        // act
+        val grid = HillClimbingAlgorithm().loadData(Path("src", "test", "resources", "Day12_TestData.txt"))
+
+        // assert
+        assertThat(grid[0]).hasSize(8)
+        assertThat(grid).hasSize(5)
+    }
 
 }
