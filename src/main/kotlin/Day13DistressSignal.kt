ce6c6b2861d74687146211e9e449bc2b42770612
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-13 20:10:22 +0100

    Started decoding

--- src/main/kotlin/Day13DistressSignal.kt
+++ src/main/kotlin/Day13DistressSignal.kt
@@ -1,15 +1,32 @@
 import java.nio.file.Path
 import kotlin.io.path.readLines
 
+typealias Packet = Pair<String, String>
+
 class DistressSignal {
 
-    fun loadData(path: Path): List<Pair<String, String>> {
+    fun loadData(path: Path): List<Packet> {
         val chunks = path.readLines().filter { it.isNotEmpty() }.chunked(2)
-        val packets = mutableListOf<Pair<String, String>>()
+        val packets = mutableListOf<Packet>()
         for (chunk in chunks) {
             packets.add(Pair(chunk[0], chunk[1]))
         }
         return packets
     }
 
+    fun decode(packet: Packet): Order {
+        var listCount = 0
+        for (index in packet.first.indices) {
+            val left = packet.first[index]
+            val right = packet.second[index]
+
+
+        }
+        return Order.WRONG
+    }
+
+}
+
+enum class Order {
+    RIGHT, WRONG
 }
--- src/test/kotlin/Day13DistressSignalTest.kt
+++ src/test/kotlin/Day13DistressSignalTest.kt
@@ -4,15 +4,26 @@ import kotlin.io.path.Path
 
 class DistressSignalTest {
 
+    private val distressSignal = DistressSignal()
+
     @Test
     fun loadData() {
-        // arrange
 
         // act
-        val packets = DistressSignal().loadData(Path("src", "test", "resources", "Day13_TestData.txt"))
+        val packets = distressSignal.loadData(Path("src", "test", "resources", "Day13_TestData.txt"))
 
         // assert
         assertThat(packets).hasSize(8)
     }
 
+    @Test
+    fun decode_packet_1() {
+
+        // act
+        val order = distressSignal.decode(Pair("[1,1,3,1,1]", "[1,1,5,1,1]"))
+
+        // assert
+        assertThat(order).isEqualTo(Order.RIGHT)
+
+    }
 }
