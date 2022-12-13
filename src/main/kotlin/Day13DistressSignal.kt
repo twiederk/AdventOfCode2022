8714d342385df68db7f64a6e5bd86eb4c9e6c944
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-13 19:53:47 +0100

    Implementd loadData

--- src/main/kotlin/Day13DistressSignal.kt
+++ src/main/kotlin/Day13DistressSignal.kt
@@ -1,3 +1,15 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
 class DistressSignal {
+
+    fun loadData(path: Path): List<Pair<String, String>> {
+        val chunks = path.readLines().filter { it.isNotEmpty() }.chunked(2)
+        val packets = mutableListOf<Pair<String, String>>()
+        for (chunk in chunks) {
+            packets.add(Pair(chunk[0], chunk[1]))
+        }
+        return packets
+    }
 
 }
--- src/test/kotlin/Day13DistressSignalTest.kt
+++ src/test/kotlin/Day13DistressSignalTest.kt
@@ -1,3 +1,18 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
 class DistressSignalTest {
+
+    @Test
+    fun loadData() {
+        // arrange
+
+        // act
+        val packets = DistressSignal().loadData(Path("src", "test", "resources", "Day13_TestData.txt"))
+
+        // assert
+        assertThat(packets).hasSize(8)
+    }
 
 }
