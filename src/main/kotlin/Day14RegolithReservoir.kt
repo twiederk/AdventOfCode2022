54c1b13b834d728d7c1331e0d73945ffc1aef413
in57kc <torsten.wiederkehr@ing.com>
2022-12-14 11:23:04 +0100

    Implemented create lines

--- src/main/kotlin/Day14RegolithReservoir.kt
+++ src/main/kotlin/Day14RegolithReservoir.kt
@@ -1,3 +1,26 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
+
 class RegolithReservoir {
 
+    fun loadData(path: Path) = path.readLines()
+
+    fun createLines(rawData: List<String>): List<Line> {
+        val lines = mutableListOf<Line>()
+        for (raw in rawData) {
+            val split = raw.split(" -> ")
+            val windows = split.windowed(2, 1)
+            for (window in windows) {
+                val (startX, startY) = window[0].split(",")
+                val (endX, endY) = window[1].split(",")
+                lines.add(Line(startX.toInt(), startY.toInt(), endX.toInt(), endY.toInt()))
+            }
+
+        }
+        return lines
+    }
+
 }
+
+data class Line(val startX: Int, val startY: Int, val endX: Int, val endY: Int)
--- src/test/kotlin/Day14RegolithReservoirTest.kt
+++ src/test/kotlin/Day14RegolithReservoirTest.kt
@@ -1,2 +1,43 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
 class RegolithReservoirTest {
+
+    // need a canvas (cave)
+    // find lowest and highest x value
+    // find lowest and highest y value
+    // create/render all rocks (lines)
+    // implement logic of falling sand.
+    // sand drops endless if max y is exceeded
+
+    private val regolithReservoir = RegolithReservoir()
+
+    @Test
+    fun loadData() {
+
+        // act
+        val rawData = regolithReservoir.loadData(Path("src", "test", "resources", "Day14_TestData.txt"))
+
+        // assert
+        assertThat(rawData).hasSize(2)
+    }
+
+    @Test
+    fun createLines() {
+        // arrange
+        val rawData = regolithReservoir.loadData(Path("src", "test", "resources", "Day14_TestData.txt"))
+
+        // act
+        val lines = regolithReservoir.createLines(rawData)
+
+        // assert
+        assertThat(lines).containsExactly(
+            Line(498,4, 498,6),
+            Line(498,6, 496,6),
+            Line(503,4, 502,4),
+            Line(502,4, 502,9),
+            Line(502,9, 494,9),
+        )
+    }
 }
