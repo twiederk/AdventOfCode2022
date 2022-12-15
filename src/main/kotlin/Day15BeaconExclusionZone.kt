b1cda001ba795a2b06293f2f47662b65a6b9d85d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-15 06:36:00 +0100

    Parse coords of sensor

--- src/main/kotlin/Day15BeaconExclusionZone.kt
+++ src/main/kotlin/Day15BeaconExclusionZone.kt
@@ -5,4 +5,26 @@ class BeaconExclusionZone {
 
     fun loadData(path: Path): List<String> = path.readLines()
 
+    fun createSensors(rawData: List<String>): List<Sensor> = rawData.map { createSensor(it) }
+
+    fun createSensor(input: String): Sensor {
+        // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        val (rawSensor, rawbeacon) = input.split(':')
+        val x = parseX(rawSensor)
+        val y = parseY(rawSensor)
+
+        return Sensor(x, y)
+
+    }
+
+    private fun parseY(rawSensor: String) =
+        rawSensor.substring("Sensor at x=".length).split(',')[1].substring(" y=".length).toInt()
+
+    private fun parseX(rawSensor: String) = rawSensor.substring("Sensor at x=".length).split(',')[0].toInt()
+
 }
+
+data class Sensor(
+    val x: Int,
+    val y: Int,
+)
--- src/test/kotlin/Day15BeaconExclusionZoneTest.kt
+++ src/test/kotlin/Day15BeaconExclusionZoneTest.kt
@@ -13,15 +13,41 @@ class BeaconExclusionZoneTest {
     // store all fields of all scans in one place
     // check row (y) how many fields are covered
 
+    private val beaconExclusionZone = BeaconExclusionZone()
+
     @Test
     fun loadData() {
-        // arrange
 
         // act
-        val rawData = BeaconExclusionZone().loadData(Path("src", "test", "resources", "Day15_TestData.txt"))
+        val rawData = beaconExclusionZone.loadData(Path("src", "test", "resources", "Day15_TestData.txt"))
 
         // assert
         assertThat(rawData).hasSize(14)
+    }
+
+    @Test
+    fun createSensors() {
+        // arrange
+        val rawData = beaconExclusionZone.loadData(Path("src", "test", "resources", "Day15_TestData.txt"))
+
+        // act
+        val sensors = beaconExclusionZone.createSensors(rawData)
+
+        // assert
+        assertThat(sensors).hasSize(14)
+    }
+
+    @Test
+    fun createSensor() {
+
+        // act
+        val sensor = beaconExclusionZone.createSensor("Sensor at x=2, y=18: closest beacon is at x=-2, y=15")
+
+        // assert
+        assertThat(sensor.x).isEqualTo(2)
+        assertThat(sensor.y).isEqualTo(18)
+//        assertThat(sensor.beaconX).isEqualTo(18)
+
     }
 
 }
