7adc7c05503b9e3ae1a770a67866155216016e70
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-15 06:21:06 +0100

    Implemented load data

--- src/main/kotlin/Day15BeaconExclusionZone.kt
+++ src/main/kotlin/Day15BeaconExclusionZone.kt
@@ -1,3 +1,8 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
 class BeaconExclusionZone {
+
+    fun loadData(path: Path): List<String> = path.readLines()
 
 }
--- src/test/kotlin/Day15BeaconExclusionZoneTest.kt
+++ src/test/kotlin/Day15BeaconExclusionZoneTest.kt
@@ -1,3 +1,27 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+
 class BeaconExclusionZoneTest {
+
+    // load data
+    // create sensor
+    // create beacon
+    // calculate Manhatten distance for each sensor
+    // calculate area for each sensor covered
+    //   by its scan using by drawing a circle with sensor as center and Manhatten distance as radius
+    // store all fields of all scans in one place
+    // check row (y) how many fields are covered
+
+    @Test
+    fun loadData() {
+        // arrange
+
+        // act
+        val rawData = BeaconExclusionZone().loadData(Path("src", "test", "resources", "Day15_TestData.txt"))
+
+        // assert
+        assertThat(rawData).hasSize(14)
+    }
 
 }
