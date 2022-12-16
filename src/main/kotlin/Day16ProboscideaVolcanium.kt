8c4d9f39402602323ef900d7b8d96634e07cec15
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-16 06:31:51 +0100

    Implemented create pipes

--- src/main/kotlin/Day16ProboscideaVolcanium.kt
+++ src/main/kotlin/Day16ProboscideaVolcanium.kt
@@ -1,3 +1,17 @@
 class ProboscideaVolcanium {
 
+    fun loadData(fileName: String): List<String> {
+        return Resources.resourceAsListOfString(fileName)
+    }
+
+    fun createPipes(rawData: List<String>): List<Pipe> =
+        rawData.map { row ->
+            Pipe(
+                row.substringAfter("Valve ").substringBefore(" has "),
+                row.substringAfter("rate=").substringBefore(";").toInt()
+            )
+        }
+
 }
+
+data class Pipe(val name: String, val flowRate: Int)
--- src/test/kotlin/Day16ProboscideaVolcaniumTest.kt
+++ src/test/kotlin/Day16ProboscideaVolcaniumTest.kt
@@ -1,3 +1,28 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class ProboscideaVolcaniumTest {
 
+    @Test
+    fun loadData() {
+
+        // act
+        val rawData = ProboscideaVolcanium().loadData("Day16_TestData.txt")
+
+        // assert
+        assertThat(rawData).hasSize(10)
+    }
+
+    @Test
+    fun createPipes() {
+        // arrange
+        val rawData = ProboscideaVolcanium().loadData("Day16_TestData.txt")
+
+        // act
+        val pipes = ProboscideaVolcanium().createPipes(rawData)
+
+        // assert
+        assertThat(pipes).hasSize(10)
+
+    }
 }
