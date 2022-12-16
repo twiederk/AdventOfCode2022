9e91b21a8df96f19f5a55fac07d8a1a0fcdbd012
in57kc <torsten.wiederkehr@ing.com>
2022-12-16 09:04:01 +0100

    Added link to reddit

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -5,6 +5,7 @@ import java.net.URI
 // =========
 // https://todd.ginsberg.com/post/advent-of-code/2022/
 // https://www.youtube.com/playlist?list=PLlFc5cFwUnmwxQlKf8uWp-la8BVSTH47J
+// https://www.reddit.com/r/adventofcode/
 
 
 /**
