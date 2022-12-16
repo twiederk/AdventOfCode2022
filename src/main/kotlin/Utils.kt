66afd73268c87fcd47d2e6fdb71bf9f777aa6d84
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-16 05:22:23 +0100

    Updated utils

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,15 @@
+// Resources
+// =========
+// https://todd.ginsberg.com/post/advent-of-code/2022/
+// https://www.youtube.com/playlist?list=PLlFc5cFwUnmwxQlKf8uWp-la8BVSTH47J
+
+
+/**
+ * Check if IntRanges overlap
+ */
 infix fun IntRange.overlaps(other: IntRange): Boolean = first <= other.last && other.first <= last
 
+/**
+ * Check if InRanges overlap fully
+ */
 infix fun IntRange.fullyOverlaps(other: IntRange): Boolean = first <= other.first && last >= other.last
