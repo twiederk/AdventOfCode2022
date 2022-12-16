0ce0f50d3ecb8e6414490ad807442f535aac4cc3
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-16 06:06:14 +0100

    Added resources to load input in different format

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.net.URI
+
 // Resources
 // =========
 // https://todd.ginsberg.com/post/advent-of-code/2022/
@@ -13,3 +16,24 @@ infix fun IntRange.overlaps(other: IntRange): Boolean = first <= other.last && o
  * Check if InRanges overlap fully
  */
 infix fun IntRange.fullyOverlaps(other: IntRange): Boolean = first <= other.first && last >= other.last
+
+internal object Resources {
+    fun resourceAsString(fileName: String, delimiter: String = ""): String =
+        resourceAsListOfString(fileName).reduce { a, b -> "$a$delimiter$b" }
+
+    fun resourceAsText(fileName: String): String =
+        File(fileName.toURI()).readText()
+
+    fun resourceAsListOfString(fileName: String): List<String> =
+        File(fileName.toURI()).readLines()
+
+    fun resourceAsListOfInt(fileName: String): List<Int> =
+        resourceAsListOfString(fileName).map { it.toInt() }
+
+    fun resourceAsListOfLong(fileName: String): List<Long> =
+        resourceAsListOfString(fileName).map { it.toLong() }
+
+    private fun String.toURI(): URI =
+        Resources.javaClass.classLoader.getResource(this)?.toURI()
+            ?: throw IllegalArgumentException("Cannot find Resource: $this")
+}
