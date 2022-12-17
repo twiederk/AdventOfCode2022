b3d8cd8600898c7e24fc1ace452f12b25f76df6a
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-17 06:33:10 +0100

    Started implementation of createNextTile

--- src/main/kotlin/Day17PyroclasticFlow.kt
+++ src/main/kotlin/Day17PyroclasticFlow.kt
@@ -1,3 +1,16 @@
 class PyroclasticFlow(val input: String) {
 
+    var towerHeight = 0
+    var shapeId = 0
+
+    fun createNextTile(): Tile {
+        return Tile(2, towerHeight + 3, shapeId)
+    }
+
 }
+
+data class Tile(
+    val x: Int,
+    val y: Int,
+    val shapeId: Int,
+)
--- src/test/kotlin/Day17PyroclasticFlowTest.kt
+++ src/test/kotlin/Day17PyroclasticFlowTest.kt
@@ -3,14 +3,26 @@ import org.junit.jupiter.api.Test
 
 class PyroclasticFlowTest {
 
+    private val pyroclasticFlow = PyroclasticFlow(Resources.resourceAsText("Day17_TestData.txt"))
+
     @Test
     fun loadData() {
-
-        // act
-        val pyroclasticFlow = PyroclasticFlow(Resources.resourceAsText("Day17_TestData.txt"))
 
         // assert
         assertThat(pyroclasticFlow.input).isEqualTo(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
     }
 
+    @Test
+    fun createNextTile() {
+        // arrange
+
+        // act
+        val tile = pyroclasticFlow.createNextTile()
+
+        // assert
+        assertThat(tile.x).isEqualTo(2)
+        assertThat(tile.y).isEqualTo(3)
+        assertThat(tile.shapeId).isEqualTo(0)
+
+    }
 }
