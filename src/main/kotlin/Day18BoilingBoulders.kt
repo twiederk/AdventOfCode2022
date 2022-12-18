311188f07e064d7316bc437a69bc30f9a677c039
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-18 13:35:12 +0100

    Calculate corner points

--- src/main/kotlin/Day18BoilingBoulders.kt
+++ src/main/kotlin/Day18BoilingBoulders.kt
@@ -1,3 +1,35 @@
 class BoilingBoulders {
 
+    fun loadData(fileName: String): List<Cube> {
+        val lines = Resources.resourceAsListOfString(fileName)
+        return lines.map { it.split(',') }.map { Cube(it[0].toInt(), it[1].toInt(), it[2].toInt()) }
+    }
+
+
+}
+
+data class Point3D(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+)
+
+data class Cube(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+) {
+    val cornerPoints = mutableListOf<Point3D>()
+
+    fun createCornerPoints() {
+        cornerPoints.add(Point3D(x - 1, y - 1, z - 1)) // A
+        cornerPoints.add(Point3D(x - 1, y, z - 1)) // B
+        cornerPoints.add(Point3D(x, y, z - 1)) // C
+        cornerPoints.add(Point3D(x, y - 1, z - 1)) // D
+        cornerPoints.add(Point3D(x - 1, y - 1, z)) // E
+        cornerPoints.add(Point3D(x - 1, y, z)) // F
+        cornerPoints.add(Point3D(x, y, z)) // G
+        cornerPoints.add(Point3D(x, y - 1, z)) // H
+
+    }
 }
--- src/test/kotlin/Day18BoilingBouldersTest.kt
+++ src/test/kotlin/Day18BoilingBouldersTest.kt
@@ -1,3 +1,58 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class BoilingBouldersTest {
 
+
+    /*
+    x,y,z
+
+    (1,1,1), (2,1,1)
+
+    (1,1,1)=>
+    8 corners:
+    1.) 0,0,0
+    2.) 1,0,0
+    3.) 0,1,0
+    4.) 0,0,1
+    5.) 1,0,1
+    6.) 1,1,0
+    7.) 0,1,1
+    8.) 1,1,1
+
+    6 sides:
+    1.)
+     */
+
+    @Test
+    fun loadData() {
+
+        // act
+        val cubes = BoilingBoulders().loadData("Day18_TestData.txt")
+
+        // assert
+        assertThat(cubes).hasSize(13)
+    }
+
+    @Test
+    fun createCornerPoints() {
+        // arrange
+        val cube = Cube(1, 1, 1)
+
+        // act
+        cube.createCornerPoints()
+
+        // assert
+        assertThat(cube.cornerPoints).containsExactly(
+            Point3D(0, 0, 0), // A
+            Point3D(0, 1, 0), // B
+            Point3D(1, 1, 0), // C
+            Point3D(1, 0, 0), // D
+            Point3D(0, 0, 1), // E
+            Point3D(0, 1, 1), // F
+            Point3D(1, 1, 1), // G
+            Point3D(1, 0, 1), // H
+        )
+
+    }
 }
