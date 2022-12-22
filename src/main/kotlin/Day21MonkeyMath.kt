11bd5cb6bd67432eb87d5de635b8dcf87765e94d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-22 06:06:39 +0100

    Start parsing data

--- src/main/kotlin/Day21MonkeyMath.kt
+++ src/main/kotlin/Day21MonkeyMath.kt
@@ -1,3 +1,28 @@
 class MonkeyMath {
+    companion object {
+        fun loadData(fileName: String): List<String> = Resources.resourceAsListOfString(fileName)
+
+        fun parseData(rawData: List<String>): Pair<List<NumberValue>, List<Equation>> {
+            val first = rawData.filter { it[it.lastIndex].isDigit() }.map { NumberValue("a", 1) }
+            val second = rawData.filter { it[it.lastIndex].isLetter() }.map { Equation(NumberValue("a", 1), NumberValue("a", 1), OperatorSign.PLUS)  }
+            return Pair(first, second)
+        }
+    }
 
 }
+
+data class NumberValue(
+    val name: String,
+    val value: Int,
+)
+
+data class Equation(
+    val number1: NumberValue,
+    val number2: NumberValue,
+    val operator: OperatorSign
+)
+
+enum class OperatorSign {
+    PLUS, MINUS, MULITPY, DIVIDE
+}
+
--- src/test/kotlin/Day21MonkeyMathTest.kt
+++ src/test/kotlin/Day21MonkeyMathTest.kt
@@ -1,3 +1,28 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class MonkeyMathTest {
 
+    @Test
+    fun loadData() {
+        // act
+        val rawData = MonkeyMath.loadData("Day21_TestData.txt")
+
+        // assert
+        assertThat(rawData).hasSize(15)
+    }
+
+    @Test
+    fun parseData() {
+        // arrange
+        val rawData = MonkeyMath.loadData("Day21_TestData.txt")
+
+        // act
+        val numbersAndEquations = MonkeyMath.parseData(rawData)
+
+        // assert
+        assertThat(numbersAndEquations.first).hasSize(8)
+        assertThat(numbersAndEquations.second).hasSize(7)
+
+    }
 }
