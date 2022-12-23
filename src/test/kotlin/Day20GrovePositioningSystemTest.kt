d8b1e296c73b4cc647bb41325fe0baa06cd9ee42
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-12-23 06:30:01 +0100

    Keep only test of aoc example

--- src/test/kotlin/Day20GrovePositioningSystemTest.kt
+++ src/test/kotlin/Day20GrovePositioningSystemTest.kt
@@ -62,51 +62,6 @@ class GrovePositioningSystemTest {
     }
 
     @Test
-    fun mix_ownExample_step2() {
-        // arrange
-        val input = listOf(1, 2, 3)
-        val grovePositioningSystem = GrovePositioningSystem(input)
-        grovePositioningSystem.mix(grovePositioningSystem.originalList[0])
-
-        // act
-        grovePositioningSystem.mix(grovePositioningSystem.originalList[1])
-
-        // assert
-        assertThat(grovePositioningSystem.originalList).extracting("value").containsExactly(1, 2, 3)
-        assertThat(grovePositioningSystem.mixList).extracting("value").containsExactly(1, 3, 2)
-    }
-
-    @Test
-    fun mix_ownExample_step3() {
-        // arrange
-        val input = listOf(1, 2, 3)
-        val grovePositioningSystem = GrovePositioningSystem(input)
-        grovePositioningSystem.mix(grovePositioningSystem.originalList[0])
-        grovePositioningSystem.mix(grovePositioningSystem.originalList[1])
-
-        // act
-        grovePositioningSystem.mix(grovePositioningSystem.originalList[2])
-
-        // assert
-        assertThat(grovePositioningSystem.originalList).extracting("value").containsExactly(1, 2, 3)
-        assertThat(grovePositioningSystem.mixList).extracting("value").containsExactly(1, 3, 2)
-    }
-
-    @Test
-    fun mixAll_ownExample() {
-        // arrange
-        val input = listOf(1, 2, 3)
-        val grovePositioningSystem = GrovePositioningSystem(input)
-
-        // act
-        grovePositioningSystem.mixAll()
-
-        // assert
-        assertThat(grovePositioningSystem.originalList).extracting("value").containsExactly(1, 2, 3)
-        assertThat(grovePositioningSystem.mixList).extracting("value").containsExactly(1, 3, 2)
-    }
-
-    @Test
     fun mixAll_aocExample_all() {
         // arrange
         val input = listOf(1, 2, -3, 3, -2, 0, 4)
@@ -247,36 +202,6 @@ class GrovePositioningSystemTest {
 
         // assert
         assertThat(grovePositioningSystem.mixList).extracting("value").containsExactly(1, 2, 3, -2, -3, 0, 4)
-    }
-
-    @Test
-    fun getNewInt_positive10() {
-
-        // act
-        val index = GrovePositioningSystem(emptyList()).getNewIndex(4, 10, 5)
-
-        // assert
-        assertThat(index).isEqualTo(4)
-    }
-
-    @Test
-    fun getNewInt_minus3() {
-
-        // act
-        val index = GrovePositioningSystem(emptyList()).getNewIndex(0, -3, 10)
-
-        // assert
-        assertThat(index).isEqualTo(6)
-    }
-
-    @Test
-    fun getNewInt_minus5() {
-
-        // act
-        val index = GrovePositioningSystem(emptyList()).getNewIndex(0, -5, 10)
-
-        // assert
-        assertThat(index).isEqualTo(4)
     }
 
     @Test
